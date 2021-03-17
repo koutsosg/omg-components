4590481ad2ac3594a875dd277f765b81602e19b5
Eleftherios Psitopoulos <lefpsi@gmail.com>
2021-03-17 21:20:39 +0100

    Add dictionary + version bump

---
+++ src/library/index.js
@@ -0,0 +1,25 @@
+import ScrollTopArrow from "./OmgBacktotop";
+import OmgCarousel from "./OmgCarousel";
+import OmgFooter1 from "./OmgFooter1";
+import OmgHeader1 from "./OmgHeader1";
+import OmgHeader2 from "./OmgHeader2";
+import LayoutSide from "./OmgLayoutSide/Layout";
+import { OmgNavigator } from "./OmgNavigator";
+import Services from "./OmgServices";
+import Services2 from "./OmgServices2";
+import Services3 from "./OmgServices3";
+import OmgSocialSmall from "./OmgSocialSmall";
+
+export default {
+  ScrollTopArrow,
+  OmgCarousel,
+  OmgFooter1,
+  OmgHeader1,
+  OmgHeader2,
+  LayoutSide,
+  OmgNavigator,
+  Services,
+  Services2,
+  Services3,
+  OmgSocialSmall,
+};
