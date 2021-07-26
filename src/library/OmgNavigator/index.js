8164a8d1b88fe6bbe58c97f0d75338fcfa133e36
koutsosg <g.koutsos123@gmail.com>
2021-07-26 16:37:24 +0300

    restore?3

--- src/library/OmgNavigator/index.js
+++ src/library/OmgNavigator/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-const getRoutes = ({ links }) => {
+const getRoutes = (links) => {
   const routes = links.map((link) => (
     <Route exact path={link.to}>
       {link.component}
